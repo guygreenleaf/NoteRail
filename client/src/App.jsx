18d6f7b38417d976db3ef59b10b20bff25a1039f
Guy <yaweh23@gmail.com>
2020-12-17 19:43:17 -0800

    fixed dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { useState } from "react";
-
-
-
-import Login from './Components/Login/LoginCard';
+import "./App.scss";
 
 function App() {
-  const [counter, count] = useState(0);
-
-
   return (
-    <div className="app-routes" style={{backgroundColor:'grey'}}>
-      <h1 onClick={()=>count(counter+1)}>{counter}</h1>
+    <div
+      className="App"
+      style={{
+        backgroundImage: 'url("https://i.imgur.com/IGrfDOy.jpg")',
+        backgroundColor: "#9DA39F",
+        // backgroundImage: 'url("https://i.imgur.com/bQFIFU6.jpg")',
+        backgroundRepeat: "no-repeat",
+        WebkitBackgroundSize: "100%",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    > 
+    
     </div>
   );
 }

b3aa0b903a28165cb728fc6926f3576a6b97f033
Guy <yaweh23@gmail.com>
2020-12-22 22:08:46 -0800

    added jwt logic

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -74,8 +74,28 @@ const userCtrl = {
 
             console.log(user)
             const refresh_token = createRefreshToken({id: user._id})
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 //7 days
+            })
 
             res.json({msg: "Login successful"})
+        } catch (error) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getAccessToken: (req, res) => {
+        try {
+            const rf_token = req.cookies.refreshtoken
+            if(!rf_token) return res.status(400).json({msg: "Please login to continue"})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err,user) => {
+                if(err) return res.status(400).json({msg: "There was an error with accessing your jwt. This is usually fixed by clearing browser data and logging back in."}) 
+                
+                const access_token = createAccessToken({id: user.id})
+                res.json({access_token})
+            })
         } catch (error) {
             return res.status(500).json({msg: err.message})
         }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -7,4 +7,7 @@ router.post('/activation', userCtrl.activateEmail)
 
 router.post('/login', userCtrl.login)
 
+router.post('/refresh_token', userCtrl.getAccessToken)
+
+
 module.exports = router

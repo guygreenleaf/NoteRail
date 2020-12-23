1a76440ea906b213d0b2f9e280f25cc800574f38
Guy <yaweh23@gmail.com>
2020-12-22 19:45:33 -0800

    hooked account creation to mongodb

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -40,8 +40,33 @@ const userCtrl = {
         } catch (err){
             return res.status(500).json({msg: err.message})
         }
+    },
+    activateEmail: async (req, res) => {
+        try {
+            const {activation_token} = req.body
+            const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET)
+
+            console.log(user)
+            const {name, email, password} = user
+
+            const check = await Users.findOne({email})
+            if(check) return res.status(400).json({msg:"This email already exists."})
+
+            const newUser = new Users({
+                name, email, password
+            })
+
+            await newUser.save()
+
+            res.json({msg: "Account has been activated."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
+
+
+
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,5 +3,6 @@ const userCtrl = require('../controllers/userCtrl')
 
 router.post('/register', userCtrl.register)
 
+router.post('/activation', userCtrl.activateEmail)
 
 module.exports = router

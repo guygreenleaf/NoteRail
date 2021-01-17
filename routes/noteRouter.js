38035018cbe260398b0d856740541ec8d8ec6120
Guy <yaweh23@gmail.com>
2021-01-16 16:50:58 -0800

    server test

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -3,8 +3,8 @@ const auth = require('../middleware/auth')
 const noteCtrl = require('../controllers/noteCtrl')
 
 router.route('/')
-    .get( noteCtrl.getNotes)
-    .post( noteCtrl.createNote)
+    .get(auth, noteCtrl.getNotes)
+    .post(auth, noteCtrl.createNote)
     
 router.route('/public')
     .get(noteCtrl.getAllNotes)
